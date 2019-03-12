b01150c8d222b94b697c8430d3255bd4e30d7d0e
rockatoo <youlia73@naver.com>
2019-03-12 13:07:02 +0900

    First commit
    Signed-off-by: rockatoo <youlia73@naver.com>

---
+++ display/source.cpp
@@ -0,0 +1,168 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <malloc.h>
+
+#include <opencv2/opencv.hpp>   
+#include <opencv2/core/core.hpp>   
+#include <opencv2/highgui/highgui.hpp>  
+
+using namespace cv;
+
+#define PI 3.14159265359
+
+typedef struct {
+	int r, g, b;
+}int_rgb;
+
+
+int** IntAlloc2(int height, int width)
+{
+	int** tmp;
+	tmp = (int**)calloc(height, sizeof(int*));
+	for (int i = 0; i<height; i++)
+		tmp[i] = (int*)calloc(width, sizeof(int));
+	return(tmp);
+}
+
+void IntFree2(int** image, int height, int width)
+{
+	for (int i = 0; i<height; i++)
+		free(image[i]);
+
+	free(image);
+}
+
+int_rgb** IntColorAlloc2(int height, int width)
+{
+	int_rgb** tmp;
+	tmp = (int_rgb**)calloc(height, sizeof(int_rgb*));
+	for (int i = 0; i<height; i++)
+		tmp[i] = (int_rgb*)calloc(width, sizeof(int_rgb));
+	return(tmp);
+}
+
+void IntColorFree2(int_rgb** image, int height, int width)
+{
+	for (int i = 0; i<height; i++)
+		free(image[i]);
+
+	free(image);
+}
+
+int** ReadImage(char* name, int* height, int* width)
+{
+	Mat img = imread(name, IMREAD_GRAYSCALE);
+	int** image = (int**)IntAlloc2(img.rows, img.cols);
+
+	*width = img.cols;
+	*height = img.rows;
+
+	for (int i = 0; i<img.rows; i++)
+		for (int j = 0; j<img.cols; j++)
+			image[i][j] = img.at<unsigned char>(i, j);
+
+	return(image);
+}
+
+void WriteImage(char* name, int** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC1);
+	for (int i = 0; i<height; i++)
+		for (int j = 0; j<width; j++)
+			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
+
+	imwrite(name, img);
+}
+
+
+void ImageShow(char* winname, int** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC1);
+	for (int i = 0; i<height; i++)
+		for (int j = 0; j<width; j++)
+			img.at<unsigned char>(i, j) = (unsigned char)image[i][j];
+	imshow(winname, img);
+	waitKey(0);
+}
+
+
+
+int_rgb** ReadColorImage(char* name, int* height, int* width)
+{
+	Mat img = imread(name, IMREAD_COLOR);
+	int_rgb** image = (int_rgb**)IntColorAlloc2(img.rows, img.cols);
+
+	*width = img.cols;
+	*height = img.rows;
+
+	for (int i = 0; i<img.rows; i++)
+		for (int j = 0; j < img.cols; j++) {
+			image[i][j].b = img.at<Vec3b>(i, j)[0];
+			image[i][j].g = img.at<Vec3b>(i, j)[1];
+			image[i][j].r = img.at<Vec3b>(i, j)[2];
+		}
+
+	return(image);
+}
+
+void WriteColorImage(char* name, int_rgb** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC3);
+	for (int i = 0; i<height; i++)
+		for (int j = 0; j < width; j++) {
+			img.at<Vec3b>(i, j)[0] = (unsigned char)image[i][j].b;
+			img.at<Vec3b>(i, j)[1] = (unsigned char)image[i][j].g;
+			img.at<Vec3b>(i, j)[2] = (unsigned char)image[i][j].r;
+		}
+
+	imwrite(name, img);
+}
+
+void ColorImageShow(char* winname, int_rgb** image, int height, int width)
+{
+	Mat img(height, width, CV_8UC3);
+	for (int i = 0; i<height; i++)
+		for (int j = 0; j<width; j++) {
+			img.at<Vec3b>(i, j)[0] = (unsigned char)image[i][j].b;
+			img.at<Vec3b>(i, j)[1] = (unsigned char)image[i][j].g;
+			img.at<Vec3b>(i, j)[2] = (unsigned char)image[i][j].r;
+		}
+	imshow(winname, img);
+
+}
+
+template <typename _TP>
+void ConnectedComponentLabeling(_TP** seg, int height, int width, int** label, int* no_label)
+{
+
+	//Mat bw = threshval < 128 ? (img < threshval) : (img > threshval);
+	Mat bw(height, width, CV_8U);
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			bw.at<unsigned char>(i, j) = (unsigned char)seg[i][j];
+	}
+	Mat labelImage(bw.size(), CV_32S);
+	*no_label = connectedComponents(bw, labelImage, 8); // 0까지 포함된 갯수임
+
+	(*no_label)--;
+
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++)
+			label[i][j] = labelImage.at<int>(i, j);
+	}
+}
+
+
+int main() {
+	int height, width;
+	int** image = ReadImage("koala.jpg", &height, &width);
+
+	for (int i = 0; i < 100; i++)
+		image[i][(100-i) / 2] = 0;
+		
+	ImageShow("test", image, height, width);
+
+	return 0;
+}
